05c085e2bc3df5ae32e8d29efafcf7243d6a6ecd
scottmains <scottmains@hotmail.co.uk>
2022-03-28 14:41:45 +0100

    updated user page

--- client/src/components/ProfileData.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ProfileData() {
-  return (
-    <div>ProfileData</div>
-  )
-}
-
-export default ProfileData
--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -8,7 +8,25 @@ const Profile = () => {
     <div>
     <Navbar />
 
-    <h1> Name: </h1>
+    <div class="h-screen bg-gray-300">
+    <div class="container flex justify-center py-20 mx-auto">
+        <div class="p-5 bg-white rounded-xl max-w-lg hover:shadow">
+            <div class="flex justify-between w-full"> 
+                <div class="ml-2">
+                    <div class="p-3">
+                        <h3 class="text-2xl">Insert Name</h3> <span> Insert Email</span>
+                    </div>
+                    <div class="flex justify-between items-center p-3 bg-gray-200 rounded-lg">
+                        <div class="mr-3"> <span class="text-gray-400 block">Wins</span> <span class="font-bold text-black text-xl">3</span> </div>
+                        <div class="mr-3"> <span class="text-gray-400 block">Losses</span> <span class="font-bold text-black text-xl">3</span> </div>
+                        <div> <span class="text-gray-400 block">Ratio</span> <span class="font-bold text-black text-xl">1</span> </div>
+                    </div>
+                </div>
+            </div>
+            <div class="flex justify-between items-center mt-2 gap-2"><button class="w-full h-12 rounded-md bg-blue-700 text-white text-md hover:shadow hover:bg-blue-800">Chat</button> </div>
+        </div>
+    </div>
+</div>
     
   </div>
   )
