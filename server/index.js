784172054ea69609d79e2f8495e07b9ecda3210e
scottmains <scottmains@hotmail.co.uk>
2022-04-14 16:29:20 +0100

    make it better

--- server/index.js
+++ server/index.js
@@ -19,26 +19,16 @@ dotenv.config();
     console.log('connected to db')
     );
 
-    var allowCrossDomain = function(req, res, next) {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-  
-      // intercept OPTIONS method
-      if ('OPTIONS' == req.method) {
-        res.send(200);
-      }
-      else {
-        next();
-      }
-  };
+   
 
 
 //Middleware
-app.use(allowCrossDomain);
+
+
+
 app.use(express.json());
 app.use(cors(corsOptions));
-app.use(cors({credentials: true, origin: true}))
+app.use(cors({credentials: true, origin: 'https://pokersociety.netlify.app/'}))
 
 app.use(express.urlencoded({extended: true}));
 app.use(bodyParser.urlencoded({extended: true}))
