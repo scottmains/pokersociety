83072fa25b285f505a14c860487ecb95ce0c8740
scottmains <scottmains@hotmail.co.uk>
2022-04-14 16:09:19 +0100

    heroku config

--- server/index.js
+++ server/index.js
@@ -19,12 +19,27 @@ dotenv.config();
     console.log('connected to db')
     );
 
+    var allowCrossDomain = function(req, res, next) {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  
+      // intercept OPTIONS method
+      if ('OPTIONS' == req.method) {
+        res.send(200);
+      }
+      else {
+        next();
+      }
+  };
+
 
 //Middleware
+app.use(allowCrossDomain);
 app.use(express.json());
-
-app.use(cors({credentials: true, origin: true}))
 app.use(cors(corsOptions));
+app.use(cors({credentials: true, origin: true}))
+
 app.use(express.urlencoded({extended: true}));
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cookieParser());
