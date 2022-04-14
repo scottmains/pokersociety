5c6c4e77d9cca2bdce8768cf91024a2d78b18a98
scottmains <scottmains@hotmail.co.uk>
2022-04-14 16:32:21 +0100

    make it better

--- server/index.js
+++ server/index.js
@@ -27,8 +27,13 @@ dotenv.config();
 
 
 app.use(express.json());
-app.use(cors(corsOptions));
-app.use(cors({credentials: true, origin: 'https://pokersociety.netlify.app/'}))
+app.use(cors({
+  origin: function(origin, callback){
+    return callback(null, true);
+  },
+  optionsSuccessStatus: 200,
+  credentials: true
+}));
 
 app.use(express.urlencoded({extended: true}));
 app.use(bodyParser.urlencoded({extended: true}))
