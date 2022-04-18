d4f07c2171fdbbc79e9ce820653234aa27fa00f4
Simon Dupkala <simon.dupkala@gmail.com>
2022-04-18 18:24:40 +0100

    second commit added surge origin

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -3,7 +3,8 @@ const allowedOrigins = [
     'http://localhost:3000',
     'http://127.0.0.1:5000',
     'https://nupokersociety.herokuapp.com/',
-    'https://pokersociety.netlify.app/'
+    'https://pokersociety.netlify.app/',
+    'https://pokersociety.surge.sh/',
 ];
 
 module.exports = allowedOrigins;
