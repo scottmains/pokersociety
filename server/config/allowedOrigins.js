c4e53eb6cf5d005831e82a3276b92973ce19d727
scottmains <scottmains@hotmail.co.uk>
2022-04-20 14:23:50 +0100

    added surge origin

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -3,7 +3,9 @@ const allowedOrigins = [
     'http://localhost:3000',
     'http://127.0.0.1:5000',
     'https://nupokersociety.herokuapp.com/',
-    'https://pokersociety.netlify.app/'
+    'https://pokersociety.netlify.app/',
+    'http://pokersociety.surge.sh/'
+
 ];
 
 module.exports = allowedOrigins;
